318ca6ab93cce9f4281359736f7fc77172423046
Shuo Wang <gken90@gmail.com>
2018-08-25 20:00:17 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -22,6 +22,7 @@ def times_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   number_of_times.times do
     puts phrase
+  end
 end
 
 def while_iterator(number_of_times)
